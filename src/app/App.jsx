6840056129fdb39716612b56b1f41fc0128ef79d
nkartchner <39930903+nkartchner@users.noreply.github.com>
2019-08-29 07:24:40 -0600

    init commit

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class App extends Component {
+  render() {
+    return (
+      <React.StrictMode>
+        <div>
+          <h1>Hello World!</h1>
+        </div>
+      </React.StrictMode>
+    );
+  }
+}
+export default App;
---
+++ src/server/server.js
@@ -0,0 +1,2 @@
+import express from 'express';
+import io from 'socket.io'
